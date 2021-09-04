7c637c9af003ff40df927936c2ef3208ec15f23f
Jonas Marcelo <marcelo.jonas@yahoo.com.br>
2021-09-04 01:09:07 -0300

    Adicionando médodo remover consulta

--- src/main/java/br/com/teste/logus/agendaconsultorio/controller/ConsultaController.java
+++ src/main/java/br/com/teste/logus/agendaconsultorio/controller/ConsultaController.java
@@ -4,10 +4,8 @@ import br.com.teste.logus.agendaconsultorio.model.ConsultaModel;
 import br.com.teste.logus.agendaconsultorio.model.MedicoModel;
 import br.com.teste.logus.agendaconsultorio.repository.ConsultaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ConsultaController {
@@ -24,4 +22,10 @@ public class ConsultaController {
     public Iterable<ConsultaModel> consultas(){
         return repository.findAll();
     }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable Long id) {
+        repository.deleteById(id.intValue());
+    }
+
 }
